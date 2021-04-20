e019560bbafe315b9f92f5ba9e5e198a33a6ff2e
Satish Prasad <spd61185@gmail.com>
2021-04-20 06:49:04 +0530

    Updated files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="myclass">
+        <h1>My First React Lab</h1>
+      </div>
+    )
+  }
+}
+export default App;
